5d35d81f7e3f40c46a787a3ca84f6ea279f9e2dd
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2022-02-11 12:09:02 +0100

    if statement

---
+++ src/golang-book/chapter5/switch.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 0
+	for i <= 10 {
+		switch i {
+			case 0: fmt.Println("Zero")
+			case 1: fmt.Println("One")
+			case 2: fmt.Println("Two")
+			case 3: fmt.Println("Three")
+			case 4: fmt.Println("Four")
+			case 5: fmt.Println("Five")
+			default: fmt.Println("Unknown Number")
+		}
+		i += 1
+	}
+}
