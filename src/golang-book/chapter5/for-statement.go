ee2c178677ef0f37757eb9eb2c5f14581ab31e1d
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2022-02-11 11:49:39 +0100

    for statement in go

--- src/golang-book/chapter5/for-statement.go
+++ src/golang-book/chapter5/for-statement.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
+/* func main() {
 	i := 1
 	for i <= 10 {
 		fmt.Println(i)
 		i += 1
+	}
+}
+*/
+// or
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
 	}
 }
