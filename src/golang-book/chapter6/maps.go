1716db71074d233032c307784489dd26e86c7e8c
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2022-02-14 16:33:01 +0100

    maps

--- src/golang-book/chapter6/maps.go
+++ src/golang-book/chapter6/maps.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 	x := make(map[string]int)
 	x["first"] = 1
 	x["second"] = 2
+
+	delete(x, "first")
+	fmt.Println(x)
+
+	name, ok := x["Uno"]
+	fmt.Println(name, ok)
+
+	y := map[string]string{
+		"firstName": "Sarah",
+		"lastName":  "Akinkunmi",
+		"age":       "Twenty-one",
+	}
+
+	fmt.Println(y["firstName"])
 }
